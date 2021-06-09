62c065a9d1d2fe943b0d1c107d29a0d6e4c6551e
tatianakharitonovich <k.tanya@tut.by>
2021-06-09 13:27:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-let inputField = document.getElementsByName("inputfield")[0];
-let resultField = document.getElementsByName("result")[0];
+let inputField = document.getElementById("disabledTextInput");
+let resultField = document.getElementById("disabledTextResult");
 
 console.log(inputField);
 
 function insertField (symbol) {
 	inputField.value = inputField.value + symbol;
-};
+}
 
 function clean() {
 	inputField.value = "";
 	resultField.value = "";
-};
+}
 
 function cleanSymbol() {
 	inputField.value = inputField.value.substr(0, inputField.value.length - 1);
-};
+}
 
 function calc() {
-	resultField.value = +eval(inputField.value).toFixed(15);
-};
+	resultField.value = +eval(inputField.value).toFixed(10);
+}
 
-inputField.addEventListener('keyup', function () {
-    inputField.value = inputField.value.replace(/[^\d()/*+=-]/,'');
+inputField.addEventListener('input', function (event) {
+    event.target.value = event.target.value.replace(/[^\d()/*+.-]/g,'');
+    checkInputEnd ();
 });
 
-inputField.addEventListener('keydown', function() {
-	if (event.key=="=") {
+inputField.addEventListener('keypress', function(event) {
+	if (event.key === "=") {
 		calc();
-		cleanSymbol();
 	} 
 });
+
+function checkInputEnd () {
+	if (event.target.value[event.target.value.length-1] === "+") ||
+	   (event.target.value[event.target.value.length-1] === "-") ||
+	   (event.target.value[event.target.value.length-1] === "*") || 
+	   (event.target.value[event.target.value.length-1] === "/") || 
+	   (event.target.value[event.target.value.length-1] === ".") {
+		event.target.value = event.target.value.replace(/[+*/.-]/g,'');
+	}
+}
 
 
